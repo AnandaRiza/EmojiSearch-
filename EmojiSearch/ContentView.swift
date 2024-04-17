4127a4defad02c39ef7324576dd27cc5b6e09293
Ananda Riza <87215017+AnandaRiza@users.noreply.github.com>
2024-04-17 09:28:35 +0700

    Version Hello Mas Rizal

--- EmojiSearch/ContentView.swift
+++ EmojiSearch/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "person")
+                .imageScale(.medium)
+                .foregroundStyle(.pink)
+            Text("Hello, Mas Rizal !")
         }
         .padding()
     }
