44a97bf2fb8ddab9b19d21c6098ec134475e3405
Ananda Riza <87215017+AnandaRiza@users.noreply.github.com>
2024-04-18 09:25:52 +0700

    TabView

---
+++ EmojiSearch/View/Tab/MainView.swift
@@ -0,0 +1,37 @@
+//
+//  MainView.swift
+//  EmojiSearch
+//
+//  Created by MACBOOK PRO on 18/04/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView{
+            ContentView()
+                .tabItem {
+                    Label("Emoji", systemImage: "ev.plug.dc.nacs")
+                }
+            
+            Text("Map View")
+                .tabItem {
+                    Label("Location", systemImage: "binoculars")
+                }
+                .badge(4444)
+            
+            Text("Setting Page")
+                .tabItem {
+                    Label("Settings", systemImage: "gear")
+                    
+                }
+
+                
+        }
+    }
+}
+
+#Preview {
+    MainView()
+}
